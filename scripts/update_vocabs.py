de60a203fd8ef345fdaf44fb33b2339cdc825ff1
Nicholas Car <nicholas.car@surroundaustralia.com>
2021-04-18 22:09:20 +1000

    Python scripts functioning

---
+++ scripts/update_vocabs.py
@@ -0,0 +1,114 @@
+from typing import List
+import argparse
+from pathlib import Path
+import httpx
+from rdflib import Graph, URIRef
+from rdflib.namespace import RDF, SKOS
+import os
+
+
+def add_vocabs(vocabs: List[Path], mappings: dict):
+    for vocab in vocabs:
+        r = httpx.post(
+            "http://fuseki.surroundaustralia.com/icsm-vocabs/data",
+            params={"graph": str(mappings[vocab.name])},
+            headers={"Content-Type": "text/turtle"},
+            content=open(vocab, "rb").read(),
+            auth=(os.environ["db_username"], os.environ["db_password"])
+        )
+        assert 200 <= r.status_code <= 300, "Status code was {}".format(r.status_code)
+
+
+def remove_vocabs(vocabs: List[Path], mappings: dict):
+    for vocab in vocabs:
+        r = httpx.post(
+            "http://fuseki.surroundaustralia.com/icsm-vocabs/update",
+            data={"update": "DROP GRAPH <{}>".format(mappings[vocab.name])},
+            auth=(os.environ["db_username"], os.environ["db_password"])
+        )
+        assert 200 <= r.status_code <= 300, "Status code was {}".format(r.status_code)
+
+
+def get_graph_uri_for_vocab(vocab: Path) -> URIRef:
+    """We can get the Graph URI for a vocab from the vocab file as we know that all VocPub-conformant vocabs
+    have one and only one ConceptScheme per file and that the ICSM VocPrez installation uses the ConceptScheme URI
+    as the Graph URI"""
+    g = Graph().parse(str(vocab), format="ttl")
+    for s in g.subjects(predicate=RDF.type, object=SKOS.ConceptScheme):
+        return s
+
+
+def get_all_vocabs_uris(vocabs: List[Path]) -> dict:
+    mappings = {}
+    for vocab in vocabs:
+        mappings[vocab.name] = get_graph_uri_for_vocab(vocab)
+
+    return mappings
+
+
+if __name__ == "__main__":
+    # for testing (until exit()):
+    # add_vocabs([Path(__file__).parent.parent / "vocabularies" / "valid.ttl"], {"valid.ttl": URIRef("http://test.com")})
+    # add_vocabs([Path(__file__).parent.parent / "vocabularies" / "valid.ttl"], {"valid.ttl": URIRef("http://test.com")})
+    # remove_vocabs([Path(__file__).parent.parent / "vocabularies" / "valid.ttl"], {"valid.ttl": URIRef("http://test.com")})
+    # exit()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m",
+        "--modified",
+        help="Vocabs to be updated in the DB",
+    )
+
+    parser.add_argument(
+        "-a",
+        "--added",
+        help="Vocabs to be added to the DB",
+    )
+
+    parser.add_argument(
+        "-r",
+        "--removed",
+        help="Vocabs to be removed from the DB",
+    )
+
+    args = parser.parse_args()
+
+    modified = []
+    if args.modified is not None:
+        for f in args.modified.split(","):
+            # if the file is in the vocabularies/ folder and ends with .ttl, it's a vocab file
+            if f.startswith("vocabularies/") and f.endswith(".ttl"):
+                modified.append(Path(f))
+
+    added = []
+    if args.added is not None:
+        for f in args.added.split(","):
+            # if the file is in the vocabularies/ folder and ends with .ttl, it's a vocab file
+            if f.startswith("vocabularies/") and f.endswith(".ttl"):
+                added.append(Path(f))
+
+    removed = []
+    if args.removed is not None:
+        for f in args.removed.split(","):
+            # if the file is in the vocabularies/ folder and ends with .ttl, it's a vocab file
+            if f.startswith("vocabularies/") and f.endswith(".ttl"):
+                removed.append(Path(f))
+
+    # map vocab files to graph_uris
+    mappings = get_all_vocabs_uris(list(set(modified + added + removed)))
+
+    # remove all removed and modified vocabs
+    remove_vocabs(removed + modified, mappings)
+
+    # add all added and modified vocabs
+    add_vocabs(added + modified, mappings)
+
+    # print for testing
+    print("modified:")
+    print([str(x) for x in modified])
+    print("added:")
+    print([str(x) for x in added])
+    print("removed:")
+    print([str(x) for x in removed])
+
---
+++ scripts/validate_vocabs.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from pyshacl import validate
+import httpx
+
+
+def main():
+    # get the validator
+    r = httpx.get("https://w3id.org/profile/vocpub/validator")
+    assert r.status_code == 200
+
+    # for all vocabs...
+    invalid_vocabs = []
+    vocabs_dir = Path(__file__).parent.parent / "vocabularies"
+    for f in vocabs_dir.iterdir():
+        # ...validate each file
+        if f.name.endswith(".ttl"):
+            v = validate(str(f), shacl_graph=r.text, shacl_graph_format="ttl")
+            if not v[0]:
+                invalid_vocabs.append(f)
+
+    # check to see if we have any invalid vocabs
+    if len(invalid_vocabs) > 0:
+        print("Invalid Vocabs:")
+        for iv in invalid_vocabs:
+            print("\t- {}".format(iv))
+
+    assert len(invalid_vocabs) == 0, "Invalid vocabs: {}".format(", ".join([str(x) for x in invalid_vocabs]))
+
+
+if __name__ == "__main__":
+    main()
